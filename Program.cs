8800f5e76ae3431780b6fe8c32c26b784fd3385c
Alejandro-TITO <73242701@idexperujapon.edu.pe>
2025-06-20 16:57:21 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICO_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+            Console.WriteLine("Ingrese la nota");
+            nota = Convert.ToInt32(Console.ReadLine());
+            if (nota >= 16)
+            {
+                Console.WriteLine("Exelente");
+            }
+            else if (nota == 14 || nota == 15)
+            {
+                Console.WriteLine("muy bueno");
+            }
+            else if (nota <= 13 && nota >= 11)
+            {
+                Console.WriteLine("Bueno");
+            }
+            else
+            {
+                Console.WriteLine("insuficiente");
+            }
+        }
+    }
+}
